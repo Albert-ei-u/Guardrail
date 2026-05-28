0db87b4e8dcefa08f7f8630751e22f853d91c5c6
albert-ei-u <uwumuremyialbert5@gmail.com>
2026-05-28 18:04:05 +0200

    pattern improved

--- src/lib.rs
+++ src/lib.rs
@@ -47,6 +47,7 @@ pub fn check_payload(input: &str) -> JsValue {
     }
 
     
+
     for pattern in xss_patterns.iter() {
         let re = Regex::new(pattern).unwrap();
         if re.is_match(input) {
